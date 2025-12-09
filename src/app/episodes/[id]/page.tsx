b08b8ab692f388ab2e89984edd455c7843d0fdaf
Ival Shamkya <ivalshamkya@gmail.com>
2025-12-09 20:57:08 +0800

    ✨feature: add runtime edge

--- src/app/episodes/[id]/page.tsx
+++ src/app/episodes/[id]/page.tsx
@@ -8,6 +8,8 @@ interface PageProps {
   };
 }
 
+export const runtime = 'edge';
+
 export default async function EpisodeDetailPage({ params }: PageProps) {
   try {
     const id = (await params).id;
